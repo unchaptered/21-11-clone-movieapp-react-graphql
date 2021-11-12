75f0d63c0549e9123b732155f543e8dcfed044e0
Unchaptered <workstation19961002@gmail.com>
2021-11-12 23:18:43 +0900

    Apply Apollo Cient

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import client from "./Apollo";
+import { ApolloProvider } from "@apollo/client";
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
